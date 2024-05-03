63f826e58714d3ef0064bba662b1f480788412db
venkatesh-jammalamadaka <jamusvenkatesh@gmail.com>
2024-05-03 13:00:02 -0400

    Add files via upload

---
+++ project1/FTPClient.java
@@ -0,0 +1,145 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class FTPClient {
+	Socket requestSocket; // socket connect to the server
+	ObjectOutputStream out; // stream write to the socket
+	ObjectInputStream in; // stream read from the socket
+	String message; // message send to the server
+	String MESSAGE; // capitalized message read from the server
+
+	void run(int serverPort, String command, String filename) {
+		try {
+			System.out.println("In run of client");
+			System.out.println("CWD: "+System.getProperty("user.dir"));
+			// create a socket to connect to the server
+			requestSocket = new Socket("localhost", serverPort);
+			System.out.println("Connected to localhost server in port " + serverPort);
+			// initialize inputStream and outputStream
+			out = new ObjectOutputStream(requestSocket.getOutputStream());
+
+			in = new ObjectInputStream(requestSocket.getInputStream());
+
+			// get Input from standard input
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+			// Send the sentence to the server
+			if (command.contains("get")) {
+				getFile(filename);
+			} else if (command.contains("upload")) {
+				sendFile(filename);
+			}
+
+		} catch (ConnectException e) {
+			System.err.println("Connection refused. You need to initiate a server first.");
+		}
+
+		catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			// Close connections
+			try {
+				in.close();
+				out.close();
+				requestSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
+
+	// send a message to the output stream
+	void getFile(String filename) {
+		try {
+			// stream write the message
+			String command_filename = "get_"+filename;
+			out.writeObject(command_filename);
+			out.flush();
+			System.out.println("Filename requested for download: " + filename);
+			// ------------------------
+			Object response = in.readObject();
+			String resp = (String) response;
+			if (resp.equalsIgnoreCase("FileNotFound")) {
+				System.out.println("Please enter valid filename present in the current dir with extension");
+				return;
+			}
+
+//			// receiving the object from server
+//			File directory = new File("C:\\Users\\venka\\eclipse-workspace\\CN\\src\\project1");
+
+			// Create a new file to save the received chunks
+			File outputFile = new File(System.getProperty("user.dir"), "new_" + filename);
+			FileOutputStream fos = new FileOutputStream(outputFile);
+
+			byte[] buffer = new byte[1000];
+			int bytesRead;
+
+			while ((bytesRead = in.read(buffer)) != -1) {
+				// Write the received chunk to the file
+				
+				fos.write(buffer, 0, bytesRead);
+
+			}
+
+			System.out.println("File received successfully from server: " + outputFile.getAbsolutePath());
+			fos.close();
+		}
+
+		catch (IOException | ClassNotFoundException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+
+	private void sendFile(String filename) {
+		String command_filename = "upload_"+filename;
+		try {
+			out.writeObject(command_filename);
+			String filePath = System.getProperty("user.dir")+ "\\" + filename;
+			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filePath));
+			byte[] buffer = new byte[1000];
+			int bytesRead;
+
+			while ((bytesRead = bis.read(buffer)) != -1) {
+				
+				out.write(buffer, 0, bytesRead);
+				out.flush();
+			}
+
+			System.out.println("File sent successfully to server: " + filename);
+			bis.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Please enter valid filename present in the current dir with extension");
+
+		} catch (IOException e) {
+			System.err.println("Error reading file or sending data: " + e.getMessage());
+		}
+	}
+
+	// main method
+	public static void main(String args[]) {
+		
+		if (args.length != 3) {
+			System.out.println("Usage: java FTPClient <server_port> <get/upload filename.extension>");
+			return;
+		}
+
+		int serverPort = Integer.parseInt(args[0]);
+		String command = args[1];
+		String filename = args[2];
+		FTPClient client = new FTPClient();
+		client.run(serverPort, command, filename);
+	}
+}
--- project1/FTPServer.java
+++ project1/FTPServer.java
@@ -1,1 +1,113 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class FTPServer {
+	
+    ServerSocket serverSocket; // ServerSocket used to listen on port number 5106
+    Socket connection = null; // Socket for the connection with the client
+    ObjectOutputStream out; // Stream write to the socket
+    ObjectInputStream in; // Stream read from the socket
+	
+	public static void main(String args[]) {
+		FTPServer s = new FTPServer();
+		s.run(5106);
+
+	}
+	
+	void run(int serverPort) {
+		System.out.println("Waiting for connection...");
+		try {
+	        serverSocket = new ServerSocket(serverPort);
+
+	        while(true) {
+	        	// Accept a connection from the client
+		        connection = serverSocket.accept();
+		        System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+
+		        // Initialize input and output streams
+		        out = new ObjectOutputStream(connection.getOutputStream());
+		        in = new ObjectInputStream(connection.getInputStream());
+		        Object obj = in.readObject();
+		        String command = (String) obj;
+		        if (command.contains("get")) {
+		        	sendFile(command.split("_")[1]);
+		        }
+		        else if (command.contains("upload")){
+		        	getFile(command.split("_")[1]);
+		        }
+                connection.close();
+	        }
+		}
+		catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	   private void sendFile(String filename) {
+	        try {
+	            String filePath = System.getProperty("user.dir")+ "\\" + filename;
+	            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(filePath));
+	            byte[] buffer = new byte[1000];
+	            int bytesRead;
+	            out.writeObject("FileFound");
+                out.flush();
+	            while ((bytesRead = bis.read(buffer)) != -1) {
+	            	
+	                out.write(buffer, 0, bytesRead);
+	                out.flush();
+	            }
+
+	            System.out.println("File sent successfully to client: " + filename);
+	            bis.close();
+	        } catch (FileNotFoundException e) {
+	            System.err.println("File not found: " + filename);
+	            try {
+	                out.writeObject("FileNotFound");
+	                out.flush();
+	            } catch (IOException ex) {
+	                ex.printStackTrace();
+	            }
+	        } catch (IOException e) {
+	            System.err.println("Error reading file or sending data: " + e.getMessage());
+	        }
+	    }
+		
+		void getFile(String filename) {
+			try {
+				// stream write the message
+				
+				System.out.println("filename about to be received: " + filename);
+				// ------------------------
+
+
+				// Create a new file to save the received chunks
+				File outputFile = new File(System.getProperty("user.dir"), "new_" + filename);
+				FileOutputStream fos = new FileOutputStream(outputFile);
+
+				byte[] buffer = new byte[1000];
+				int bytesRead;
+
+				while ((bytesRead = in.read(buffer)) != -1) {
+					// Write the received chunk to the file
+					
+					fos.write(buffer, 0, bytesRead);
+
+				}
+
+				System.out.println("File received successfully from client: " + outputFile.getAbsolutePath());
+				fos.close();
+			}
+
+			catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+}
